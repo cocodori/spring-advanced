d826a0fe00b727f2b974cf780c71a169dde76c44
hoon <moradulee@gmail.com>
2022-06-19 00:13:09 +0900

    프록시 패턴 예제 2

--- src/test/java/hello/advanced/trace/proxy/pureproxy/ProxyPatternTest.java
+++ src/test/java/hello/advanced/trace/proxy/pureproxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.advanced.trace.proxy.pureproxy;
 
+import hello.advanced.trace.proxy.pureproxy.code.CacheProxy;
 import hello.advanced.trace.proxy.pureproxy.code.ProxyPatternClient;
 import hello.advanced.trace.proxy.pureproxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,17 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
         client.execute();
         client.execute();
         client.execute();
