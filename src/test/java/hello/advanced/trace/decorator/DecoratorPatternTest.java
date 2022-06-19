0930c82c9b0f6252ab377f958ae8784954ec4447
hoon <moradulee@gmail.com>
2022-06-19 21:59:47 +0900

    데코레이터 패턴 예제 2

--- src/test/java/hello/advanced/trace/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/advanced/trace/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package hello.advanced.trace.decorator;
 
 import hello.advanced.trace.decorator.code.Component;
 import hello.advanced.trace.decorator.code.DecoratorPatternClient;
+import hello.advanced.trace.decorator.code.MessageDecorator;
 import hello.advanced.trace.decorator.code.RealComponent;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,15 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component component = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(component);
+
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
 
         client.execute();
     }
