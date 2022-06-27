87f5ec1f27d1a7d8f78d54e763fc39291b728c0d
cocodori <moradulee@gmail.com>
2022-06-27 18:43:22 +0900

    빈 후처리기 적용

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -8,6 +8,7 @@ import hello.advanced.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.advanced.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.advanced.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -22,7 +23,8 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app")
 public class AdvancedApplication {
 	public static void main(String[] args) {
