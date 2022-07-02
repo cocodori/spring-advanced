b169405ee137b2bc24e96856ed560f9b864f6761
cocodori <moradulee@gmail.com>
2022-07-02 15:44:42 +0900

    this, target

---
+++ src/test/java/hello/advanced/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,57 @@
+package hello.advanced.aop.pointcut;
+
+import hello.advanced.aop.order.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(ThisTargetTest.ThisTargetAspect.class)
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=false") //JDK 동적 프록시
+@SpringBootTest(properties = "spring.aop.proxy-target-class=true") //JDK 동적 프록시
+public class ThisTargetTest {
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class ThisTargetAspect {
+        @Around("this(hello.advanced.aop.order.aop.member.MemberService)")
+        public Object doThisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.advanced.aop.order.aop.member.MemberService)")
+        public Object doTargetInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.advanced.aop.order.aop.member.MemberServiceImpl)")
+        public Object doTarget(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+
+        @Around("this(hello.advanced.aop.order.aop.member.MemberServiceImpl)")
+        public Object doThis (ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
+
+}
